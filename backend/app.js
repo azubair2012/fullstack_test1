c3480c97afdfb70c21fe5dc966bebe087ba88c4f
azubair2012 <studio.zubair@gamil.com>
2023-08-26 16:50:49 +0600

    mat-wind added

--- backend/app.js
+++ backend/app.js
@@ -1,2 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv").config(); // to use.env file for environment variables.
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
---
+++ frontend/app/data/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Link from "next/link";
+
+const Datapull = () => {
+  return (
+    <div>
+      <Link href="/">home</Link>
+    </div>
+  );
+};
+
+export default Datapull;
